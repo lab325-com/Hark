0d611cf37d7fdf0d07b84e34195c15c2d010fa95
Alex <alex.terode@gmail.com>
2021-12-29 15:18:29 +0200

    Hark-2
    Prepare setup project

---
+++ Hark/Hark/AppDelegate/AppDelegate.swift
@@ -0,0 +1,15 @@
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        
+        return RootRouter.sharedInstance.application(didFinishLaunchingWithOptions: launchOptions as [UIApplication.LaunchOptionsKey: Any]?, window: window ?? UIWindow(frame: UIScreen.main.bounds))
+    }
+}
+
---
+++ Hark/Hark/Controllers/SplashController/SplashController.swift
@@ -0,0 +1,10 @@
+
+import UIKit
+
+class SplashController: BaseController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
