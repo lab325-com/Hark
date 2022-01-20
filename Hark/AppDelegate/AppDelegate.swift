656c8204400d8b26a158739d2c01b21da829fd26
Alex <alex.terode@gmail.com>
2022-01-20 17:00:00 +0200

    Hark-15

--- Hark/AppDelegate/AppDelegate.swift
+++ Hark/AppDelegate/AppDelegate.swift
@@ -1,5 +1,6 @@
 
 import UIKit
+import AgoraRtcKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -7,7 +8,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
         
         return RootRouter.sharedInstance.application(didFinishLaunchingWithOptions: launchOptions as [UIApplication.LaunchOptionsKey: Any]?, window: window ?? UIWindow(frame: UIScreen.main.bounds))
     }
